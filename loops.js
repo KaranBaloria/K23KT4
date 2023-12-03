75d365be306cc9863e7fb1fecd923f070815a151
KaranBaloria <149923959+KaranBaloria@users.noreply.github.com>
2023-12-03 17:34:02 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,27 @@
+// for(initialization; condition; updation)       # for loop
+//         i=0           i<10       i++
+
+
+// let a = ["Java", "Python", "C", "C++"]
+// let len_a = a.length
+// let s = ""
+// for(let i=0; i<len_a; i++){
+//     s+=a[i] + " "
+
+// }
+// document.getElementById("demo").innerHTML = s
+
+
+
+
+// while loop
+
+let a1 = ["Thor", "Loki", "Hawk", "Hulk"]
+let len_a1=a1.length
+let s1=""
+let j=0
+while(j<len_a1){
+    s1+=a1[j] + " "
+    j++
+}
+document.getElementById("demo").innerHTML= s1    
